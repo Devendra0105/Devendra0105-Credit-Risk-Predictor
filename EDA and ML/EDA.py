96ac54edac7adde1d67986c63bd2d40d93fb2a7f
Devendra Kumar Gehlot <devendragehlot0125@gmail.com>
2026-03-05 10:14:18 +0530

    change

--- EDA and ML/EDA.py
+++ EDA and ML/EDA.py
@@ -1,28 +1,16 @@
-'''EDA feature engineering and data preprocessing file '''
+'''EDA feature engineering and data preprocessing file
+used to understand data and make it good for macchine learning  '''
 def EDA_part():
 
     import pandas as pd
     import numpy as np
     data=pd.read_csv('dataset/german_credit_data.csv')
     df=pd.DataFrame(data)
-    # print(df.columns.tolist())
-    # print(df.head(10))
-    # print(df.describe())
-    # print('\nStatus_account : ',df['status_account'].unique().tolist())
-    # print('\ncredit History : ',df['credit_history'].unique().tolist())
-    #print('\npurpose ',df['purpose'].unique().tolist())
-    # print('\nstatus savings : ',df['status_savings'].unique().tolist())
-    # print('\nyears employement : ',df['years_employment'].unique().tolist())
-    # print('\nstatus and sex : ',df['status_and_sex'].unique().tolist())
-    # print('\nsecondary obligator : ',df['secondary_obligor'].unique().tolist())
-    #   print('\ncollateral : ',df['collateral'].unique().tolist())
-    # print('\nother installment plans : ',df['other_installment_plans'].unique().tolist())
-    # print('\nhousing : ',df['housing'].unique().tolist())
-    # print('\njob : ',df['job'].unique().tolist())
-    # print('\ntelephone : ',df['telephone'].unique().tolist())
-    # print('\nis foreign worker : ',df['is_foreign_worker'].unique().tolist())
-    # print('\ntarget : ',df['target'].unique().tolist())
-
+    '''
+    print(df.describe())
+    print(df.isnull().sum())
+    print(df.dtypes())
+    '''
     df[['gender','martial Status']]=df['status_and_sex'].str.split(' : ',expand=True)
     df.drop('status_and_sex',axis=1, inplace=True)
     # print(df.columns)
--- EDA and ML/grid_searchCV.py
+++ EDA and ML/grid_searchCV.py
@@ -1,3 +1,4 @@
+'''This file contains grid searcch CV method used to find best model that do not overfit and gives high accuracy and recall '''
 from EDA import EDA_part
 from sklearn.model_selection import train_test_split
 from sklearn.model_selection import GridSearchCV
@@ -8,9 +9,13 @@ from sklearn.neighbors import KNeighborsClassifier
 from sklearn.linear_model import LogisticRegression
 from xgboost import XGBClassifier
 from sklearn.metrics import accuracy_score, classification_report, confusion_matrix, roc_auc_score
+from sklearn.preprocessing import StandardScaler
 
 new=EDA_part()
 X=new.drop('target',axis=1)
 y=new['target']
-print(X)
-print(y)
+X_train,X_test,y_train,y_test=train_test_split(X,y, test_size=0.2, random_state=42)
+scaler=StandardScaler()
+X_train_scaled=scaler.fit_transform(X_train)
+x_test_scaled=scaler.fit(X_test)
+
